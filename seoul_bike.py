29458db43d85737e9434890defbace29e4ec76c1
심재호 <jhshim@ucsd.edu>
2026-07-25 17:10:45 +0900

    json to DataFrame using API

--- seoul_bike.py
+++ seoul_bike.py
@@ -1,17 +1,43 @@
 import requests
-
-# API Key and URL for Seoul Bike Rental Data
-API_Key = '444d424e786a61653930524d624872'
-url = 'http://openapi.seoul.go.kr:8088/' + API_Key + '/json/tbCycleRentUseTimeInfo/1/5/20221002/9'
-
-# Make a GET request to the API
-response = requests.get(url)
-if response.status_code == 200:
-    data = response.json()
-
-# Transform JSON data into a DataFrame
 import pandas as pd
-data_df = pd.DataFrame(data['cycleRentUseTimeInfo']['row'])
 
-print(data_df.head())
+API_Key = '444d424e786a61653930524d624872'
 
+def get_daily_data(date):
+  count_url = f'http://openapi.seoul.go.kr:8088/{API_Key}/json/tbCycleRentUseDayInfo/1/2/{date}'
+
+  count_response = requests.get(count_url, timeout=30) 
+  if count_response.status_code == 200:
+    count_data = count_response.json()
+  else: 
+    return "Request Failed"
+  
+  total_count = int(count_data['cycleRentUseDayInfo']['list_total_count'])
+
+  entire_rows = []
+  start = 1
+  page_size = 1000
+  
+  while start <= total_count:
+    end = start + page_size - 1
+    if end > total_count:
+      end = total_count
+    
+    url = f'http://openapi.seoul.go.kr:8088/{API_Key}/json/tbCycleRentUseDayInfo/{start}/{end}/{date}'
+
+    response = requests.get(url, timeout=30)
+    if response.status_code == 200:
+      data = response.json()
+    else: 
+      return "Request Failed"
+
+    rows = data['cycleRentUseDayInfo']['row']
+
+    entire_rows.extend(rows)
+    start = end + 1
+  
+  data_df = pd.DataFrame(entire_rows)
+  
+  return data_df
+
+print(get_daily_data('20230101'))
